6ec477c81787f21a2ce9c8a4cbc2e52f8a29fac0
Mateusz Lapsa-Malawski <m@cr3.io>
2017-11-21 14:58:37 +0000

    configured for great

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: locust -H https://www.great.gov.uk/ --web-host=0.0.0.0 -P $PORT
---
+++ locustfile.py
@@ -0,0 +1,18 @@
+from locust import HttpLocust, TaskSet, task
+
+
+class WebsiteTasks(TaskSet):
+
+    @task
+    def index(self):
+        self.client.get("/uk")
+
+    @task
+    def about(self):
+        self.client.get("/invest")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    min_wait = 5000
+    max_wait = 15000
